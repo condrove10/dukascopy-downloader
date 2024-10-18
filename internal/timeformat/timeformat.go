87dbf90ebf05d64e64b56cde5283a36ec61b81bc
condrove10 <condrove10@protonmail.com>
2024-10-18 13:49:45 +0200

    minor timeformat changes

--- internal/timeformat/timeformat.go
+++ internal/timeformat/timeformat.go
@@ -12,6 +12,10 @@ func GetDateTimeRange(start, end time.Time, hourInterval int) []time.Time {
 		end = time.Date(end.Year(), end.Month(), end.Day(), end.Hour()-1, 0, 0, 0, end.Location())
 	}
 
+	if end.After(now) {
+		end = time.Date(currentHour.Year(), currentHour.Month(), currentHour.Day(), currentHour.Hour(), 0, 0, 0, currentHour.Location())
+	}
+
 	var times []time.Time
 	for t := time.Date(start.Year(), start.Month(), start.Day(), start.Hour(), 0, 0, 0, start.Location()); t.Before(end); t =
 		t.Add(time.Duration(hourInterval) * time.Hour) {
